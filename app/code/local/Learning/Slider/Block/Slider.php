2bd156001621a40b553d18c88cc19c5fb113042b
Florian Tournay <florian.tournay@hotmail.fr>
2016-06-22 12:39:09 +0200

    first commit

---
+++ app/code/local/Learning/Slider/Block/Slider.php
@@ -0,0 +1,15 @@
+<?php
+
+class Learning_Slider_Block_Slider extends Mage_Core_Block_Template
+{
+    public function getSlides()
+    {
+        $slides = Mage::getModel('learning_slider/slide')
+            ->getCollection()
+            ->addIsActiveFilter()
+            ->addOrderByPosition();
+        return $slides;
+    }
+
+
+}
---
+++ app/code/local/Learning/Slider/controllers/indexController.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of indexController
+ *
+ * @author sparttan
+ */
+class Learning_Slider_indexController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+}
