3ff89069f3e95fa0fc8e1fbd451a208eebb4cdf6
mauriau <maxime.auriau@gmail.com>
2016-06-24 08:30:53 +0200

    Essai de lier le produit et l'entity

--- app/code/local/Learning/Domaine/Block/Adminhtml/Cepage/Edit/Tabs.php
+++ app/code/local/Learning/Domaine/Block/Adminhtml/Cepage/Edit/Tabs.php
@@ -10,4 +10,16 @@ class Learning_Domaine_Block_Adminhtml_Cepage_Edit_Tabs extends Mage_Adminhtml_B
         $this->setDestElementId('edit_form');
         $this->setTitle(Mage::helper('learning_domaine')->__('Cepage Information'));
     }
+
+    protected function _beforeToHtml()
+    {
+        $this->addTab('form_section', array(
+            'label' => Mage::helper('learning_domaine')->__('Domaines liés'),
+            'title' => Mage::helper('learning_domaine')->__('Domaines liés'),
+            'content' => $this->getLayout()->createBlock('learning_domaine/adminhtml_cepage_edit_tab_form')->toHtml()
+        ));
+
+        return parent::_beforeToHtml();
+    }
+
 }
